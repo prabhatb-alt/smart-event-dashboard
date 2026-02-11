57d3c44d667d6cebc8c750b18609ca2907d947f8
Prabhat Bhatia <prabhat@Prabhats-MacBook-Pro.local>
2026-02-11 22:08:33 +0530

    Completed dashboard as based on screenshots, used custom gradients(https://webgradients.com/)

--- script.js
+++ script.js
@@ -25,3 +25,46 @@ document.addEventListener('keydown',(event) =>  {
     document.querySelector('.key').innerText = event.key
 
 })
+
+eventCards.addEventListener('click', function(e) {
+    if (e.target.classList.contains('deleteCard')) {
+        e.target.parentElement.remove()
+    }
+})
+
+
+const clearBtn = document.querySelector('.events button:first-child')
+clearBtn.addEventListener('click', function() {
+    eventCards.innerHTML = ""
+})
+
+const sampleBtn = document.querySelector('.events button:nth-child(2)')
+sampleBtn.addEventListener('click', function() {
+
+    const samples = [
+        {title: "Documentation", date: "2025-08-23", cat: "Meetup", desc: "KRMU Documentation Day"},
+        {title: "Birthday", date: "2025-10-11", cat: "Social", desc: "18th Birthday"}
+    ]
+
+    samples.forEach(sample => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML = `
+            <h2>${sample.title}</h2>
+            <p>${sample.date}</p>
+            <button>${sample.cat}</button>
+            <p>${sample.desc}</p>
+            <div class="deleteCard">X</div>
+        `
+        eventCards.appendChild(card)
+    })
+})
+
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('card')){
+        console.log("Parent:", e.target.parentElement)
+        console.log("First Child:", e.target.firstElementChild)
+        console.log("Next Sibling:", e.target.nextElementSibling)
+        console.log("Children:", e.target.children)
+    }
+})
